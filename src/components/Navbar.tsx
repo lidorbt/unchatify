3a31cf681012f07f1c5c9c1e03f55cff9be9f879
lidorbt <lidorb98@gmail.com>
2024-12-05 23:01:45 +0200

    update logo

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { Wand2 } from 'lucide-react';
+import brainLogo from '../assets/brain.png'
 
 const Navbar: React.FC = () => {
   return (
     <nav className="fixed top-0 left-0 w-full bg-white shadow-md z-10">
       <div className="container mx-auto px-6 py-3 flex justify-between items-center">
-        <Link to="/" className="flex items-center text-xl font-bold text-gray-800">
-          <Wand2 className="mr-2 text-purple-600" /> AI Text Obfuscator
+        <Link to="/" className="flex items-center text-xl font-bold text-gray-800 gap-x-4">
+          <img src={brainLogo} alt='logo' className='w-8 h-8' />
+          AI Text Obfuscator
         </Link>
         <div>
           <Link to="/" className="text-gray-600 hover:text-purple-600 mx-4">Home</Link>
